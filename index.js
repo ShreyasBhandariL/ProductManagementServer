5ad5d1b91073e5de94741709dce11c46794878ed
ShreyasBhandariL <shreyasbhandaricse@gmail.com>
2024-06-30 21:32:53 +0530

    Cors updated

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ connectDb();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://productservice.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.post("/register", async (req, res) => {
   try {
