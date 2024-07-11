1f8e7544609c2c9cb601d17507b2a74c500e9694
ShreyasBhandariL <shreyasbhandaricse@gmail.com>
2024-07-11 19:53:22 +0530

    Added Category to buyer

--- models/Buyer.js
+++ models/Buyer.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const buyerSchema = new mongoose.Schema({
   productId: { type: String, required: true },
   productName: { type: String, required: true },
-  productPrice: { type: Number, required: true },
-  productCategory: { type: Number, required: true },
+  productPrice: { type: String, required: true },
+  productCategory: { type: String, required: true },
   quantity: { type: Number, required: true },
   buyerName: { type: String, required: true },
   buyerContact: { type: String, required: true },
